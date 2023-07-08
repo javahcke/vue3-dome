7ad9f4d5edcad2ada22abb56bd0e9fd99a845cd3
yuming_yang <1024217025@qq.com>
2023-07-08 18:45:46 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,48 @@
 <template>
-    <div id="app">
-        <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
-        <!-- {{ count }}
-    {{ double }} -->
-        <!-- <ul>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    {{ count }}
+    {{ double }}
+    <p>{{ error }}</p>
+    <ul>
         <li v-for="number in numbers" :key="number">{{ number }}</li>
-    </ul> -->
-        <!-- <button @click="openModal">打开</button> -->
-        <ModalBox :isOpen="modalIsOpen" @close-modal="onMoadlClose"
-            >my modal !!!!</ModalBox
-        >
-        <!-- {{ person.name }} -->
-        <!-- <h1>X:{{ x }}</h1>
-    <h1>Y:{{ y }}</h1> -->
-        <!-- <button @click="increase">+1</button>
-    <button @click="ipdataGerrting">+1</button> -->
-        <!-- <h1 v-if="loading">加装中....</h1> -->
-        <Suspense>
-            <template #default>
-                <div>
-                    <async-show />
-                    <dog-show />
-                </div>
-            </template>
-            <template #fallback>
-                <p>loading......</p>
-            </template>
-        </Suspense>
+    </ul>
+    <button @click="openModal">打开</button>
+    <ModalBox :isOpen="modalIsOpen" @close-modal="onMoadlClose">my modal !!!!</ModalBox>
+    {{ person.name }}
+    <h1>X:{{ x }}</h1>
+    <h1>Y:{{ y }}</h1>
+    <button @click="increase">+1</button>
+    <button @click="ipdataGerrting">+1</button>
+    <h1 v-if="loading">加装中....</h1>
+    <Suspense>
+      <template #default>
+        <div>
+          <async-show />
+          <dog-show />
+        </div>
+      </template>
+      <template #fallback>
+        <p>loading......</p>
+      </template>
+    </Suspense>
 
-        <!-- <img v-if="loaded" :src="resule[0].url" /> -->
-    </div>
+    <!-- <img v-if="loaded" :src="resule[0].url" /> -->
+  </div>
 </template>
 
 <script lang="ts">
 import {
-    ref,
-    computed,
-    reactive,
-    toRefs,
-    onUpdated,
-    onRenderTriggered,
-    watch,
-    onMounted,
-    onUnmounted,
+  ref,
+  computed,
+  reactive,
+  toRefs,
+  onUpdated,
+  onRenderTriggered,
+  watch,
+  onMounted,
+  onUnmounted,
+  onErrorCaptured
 } from "vue";
 import useMousePosition from "./hooks/useMousePosition";
 import useURLLoader from "./hooks/useURLLoader";
@@ -50,97 +50,103 @@ import ModalBox from "./components/ModalBox.vue";
 import AsyncShow from "./components/AsyncShow.vue";
 import DogShow from "./components/DogShow.vue";
 interface DataProps {
-    count: number;
-    double: number;
-    increase: () => void;
-    numbers: number[];
-    person: { name?: string };
+  count: number;
+  double: number;
+  increase: () => void;
+  numbers: number[];
+  person: { name?: string };
 }
 interface DogResult {
-    message: string;
-    status: string;
+  message: string;
+  status: string;
 }
 interface CatResult {
-    id: string;
-    url: string;
-    width: number;
-    height: number;
+  id: string;
+  url: string;
+  width: number;
+  height: number;
 }
 export default {
-    name: "App",
-    components: {
-        ModalBox,
-        AsyncShow,
-        DogShow,
-    },
-    setup() {
-        onUpdated(() => {
-            console.log("updated");
-        });
-        onRenderTriggered((event) => {
-            console.log(event);
-        });
-        const data: DataProps = reactive({
-            count: 0,
-            increase: () => {
-                data.count++;
-            },
-            double: computed(() => data.count * 2),
-            numbers: [0, 1, 2],
-            person: { name: "Joe" },
-        });
+  name: "App",
+  components: {
+    ModalBox,
+    AsyncShow,
+    DogShow,
+  },
+  setup() {
+    const error = ref(null)
+    onErrorCaptured((e: any) => {
+      error.value = e
+      return true
+    })
+    onUpdated(() => {
+      console.log("updated");
+    });
+    onRenderTriggered((event) => {
+      console.log(event);
+    });
+    const data: DataProps = reactive({
+      count: 0,
+      increase: () => {
+        data.count++;
+      },
+      double: computed(() => data.count * 2),
+      numbers: [0, 1, 2],
+      person: { name: "Joe" },
+    });
 
-        data.numbers[0] = 5;
-        data.person.name = "v";
-        const refData = toRefs(data);
-        const greetiongs = ref("");
-        const ipdataGerrting = () => {
-            greetiongs.value += "Hello! ";
-        };
+    data.numbers[0] = 5;
+    data.person.name = "v";
+    const refData = toRefs(data);
+    const greetiongs = ref("");
+    const ipdataGerrting = () => {
+      greetiongs.value += "Hello! ";
+    };
 
-        watch([greetiongs, () => data.count], (newValue, oldValue) => {
-            console.log(newValue);
-            console.log(oldValue);
-            document.title = "updated" + greetiongs.value + data.count;
-        });
+    watch([greetiongs, () => data.count], (newValue, oldValue) => {
+      console.log(newValue);
+      console.log(oldValue);
+      document.title = "updated" + greetiongs.value + data.count;
+    });
 
-        const { x, y } = useMousePosition();
-        const { resule, loading, loaded } = useURLLoader<CatResult[]>(
-            // "https://dog.ceo/api/breeds/image/random"
-            "https://api.thecatapi.com/v1/images/search?limit=1"
-        );
-        watch(resule, () => {
-            if (resule.value) {
-                console.log(resule.value[0].url);
-            }
-        });
-        const modalIsOpen = ref(false);
-        const openModal = () => (modalIsOpen.value = true);
-        const onMoadlClose = () => (modalIsOpen.value = false);
-        return {
-            ...refData,
-            greetiongs,
-            ipdataGerrting,
-            x,
-            y,
-            resule,
-            loading,
-            loaded,
-            modalIsOpen,
-            openModal,
-            onMoadlClose,
-        };
-    },
+    const { x, y } = useMousePosition();
+    const { resule, loading, loaded } = useURLLoader<CatResult[]>(
+      // "https://dog.ceo/api/breeds/image/random"
+      "https://api.thecatapi.com/v1/images/search?limit=1"
+    );
+    watch(resule, () => {
+      if (resule.value) {
+        console.log(resule.value[0].url);
+      }
+    });
+    const modalIsOpen = ref(false);
+    const openModal = () => (modalIsOpen.value = true);
+    const onMoadlClose = () => (modalIsOpen.value = false);
+    return {
+      ...refData,
+      greetiongs,
+      ipdataGerrting,
+      x,
+      y,
+      resule,
+      loading,
+      loaded,
+      modalIsOpen,
+      openModal,
+      onMoadlClose,
+      error
+    };
+  },
 };
 </script>
 
 <style>
 #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
--- src/components/DogShow.vue
+++ src/components/DogShow.vue
@@ -1,20 +1,17 @@
 <template>
-    <img :src="result && result.message">
+  <img :src="result && result.message">
 </template>
 <script lang="ts">
 import axios from "axios";
 import { defineComponent } from "vue";
 export default defineComponent({
-    
-    
-    async setup(){
-        
-        console.log(11111111)
-        const rawData = await axios.get('https://dog.ceo/api/breeds/image/random')
-        console.log(11111111)
-        return {
-            result: rawData.data
-        }
+  async setup() {
+    console.log(11111111)
+    const rawData = await axios.get('https://dog.ceo/api/breeds/image/random')
+    console.log(11111111)
+    return {
+      result: rawData.data
     }
+  }
 });
 </script>
